4cc4f8b42346f728bcc8df56e82b998535349978
LiubovShuliak <shuliak.lyubov@gmail.com>
2025-03-16 02:38:10 +0200

    init

--- client/src/views/auth/SignIn.tsx
+++ client/src/views/auth/SignIn.tsx
@@ -43,9 +43,6 @@ export default function SignIn() {
           Sign In
         </h4>
         <div id={'buttonDiv'}></div>
-        <button className="linear mt-2 w-full rounded-xl bg-brand-500 py-[12px] text-base font-medium text-white transition duration-200 hover:bg-brand-600 active:bg-brand-700 dark:bg-brand-400 dark:text-white dark:hover:bg-brand-300 dark:active:bg-brand-200">
-          Sign In
-        </button>
       </div>
     </div>
   );
